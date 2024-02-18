74dcbdb99a68addf90f0131cde8b4d90cc468347
Teacher Koeks <k.a.b.koeks@gmail.com>
2024-02-18 18:38:34 +0100

    DB/config en Model.php

---
+++ config/config.php
@@ -0,0 +1,6 @@
+<?php
+// Database Configuration
+define("DB_HOST", "localhost");
+define("DB_NAME", "studenthousing");
+define("DB_USER", "user");
+define("DB_PASS", "password");
---
+++ core/Database.php
@@ -0,0 +1,20 @@
+<?php
+
+class Database {
+    private $host = DB_HOST; // Defined in config.php
+    private $db_name = DB_NAME; // Defined in config.php
+    private $username = DB_USER; // Defined in config.php
+    private $password = DB_PASS; // Defined in config.php
+    public $conn;
+
+    public function getConnection() {
+        $this->conn = null;
+        try {
+            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+            $this->conn->exec("set names utf8");
+        } catch(PDOException $exception) {
+            echo "Connection error: " . $exception->getMessage();
+        }
+        return $this->conn;
+    }
+}
